dddc7a264f4d9a0a140e1d4f784fffc4955ee5be
zeusgolohor <zeusgolohor@gmail.com>
2024-02-21 19:17:39 -0800

    Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
    Requirements:
    
    You must use urllib or requests module
    The script must accept an integer as a parameter, which is the employee ID
    The script must display on the standard output the employee TODO list progress in this exact format:
    First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
    EMPLOYEE_NAME: name of the employee
    NUMBER_OF_DONE_TASKS: number of completed tasks
    TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
    Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
    Example:

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A script that, using this REST API, for a given
+employee ID, returns information about his/her
+TODO list progress.
+"""
+import requests
+import sys
+
+
+def get_todo(id=None):
+    """
+    A method to get an eployee's todo list.
+    """
+    if (id is not None):
+        res = requests.get('https://jsonplaceh\
+                older.typicode.com/users/{}/todos'.format(id))
+        user = requests.get('https://jsonplaceh\
+                older.typicode.com/users/{}'.format(id))
+        done = []
+        all = []
+        if (res.status_code == 200):
+            user = user.json()
+            for value in res.json():
+                if (value['completed'] is True):
+                    done.append(value['title'])
+                all.append(value['title'])
+            print('Employee {} is done with tasks({}/{}):'
+                  .format(user['name'], len(done), len(all)))
+            for title in done:
+                print('\t {}'.format(title))
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) >= 1):
+        id = sys.argv[1]
+        get_todo(id)
