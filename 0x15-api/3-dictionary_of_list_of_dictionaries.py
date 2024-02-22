4b3b3840464945b29e17631e5e55fafb7e1e82a4
zeusgolohor <zeusgolohor@gmail.com>
2024-02-21 20:32:47 -0800

    Using what you did in the task #0, extend your Python script to export data in the JSON format.
    Requirements:
    
    Records all tasks from all employees
    Format must be: { USER_ID: [ {username: USERNAME, task: TASK_TITLE, completed: TASK_COMPLETED_STATUS}, {username: USERNAME, task: TASK_TITLE, completed: TASK_COMPLETED_STATUS}, ... ], USER_ID: [ {username: USERNAME, task: TASK_TITLE, completed: TASK_COMPLETED_STATUS}, {username: USERNAME, task: TASK_TITLE, completed: TASK_COMPLETED_STATUS}, ... ]}
    File name must be: todo_all_employees.json
    Example:

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+A script to get tasks from all employees
+and exports the data to JSON files.
+"""
+import requests
+import json
+
+
+def get_todo_for_all_users():
+    """
+    A method to get tasks from all employees
+    """
+    # user id range for look up.
+    user_ids = range(1, 11)
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'
+    fileName = 'todo_all_employees.json'
+    all = []
+
+    for user_id in user_ids:
+        r = requests.get(url.format(user_id))
+        if r.status_code == 200:
+            user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                                .format(user_id))
+            if user.status_code == 200:
+                user_data = user.json()
+                tasks = []
+                for task in r.json():
+                    task_info = {
+                        "username": user_data['username'],
+                        "task": task['title'],
+                        "completed": task['completed']
+                    }
+                    tasks.append(task_info)
+                json_data = {str(user_id): tasks}
+                all.append(json_data)
+            else:
+                print("Error: Failed to retrieve user data for user ID:",
+                      user_id)
+        else:
+            print("Error: Failed to retrieve TODO list for user ID:", user_id)
+    with open(fileName, mode='w') as file:
+        json.dump(all, file)
+
+
+if __name__ == '__main__':
+    get_todo_for_all_users()
