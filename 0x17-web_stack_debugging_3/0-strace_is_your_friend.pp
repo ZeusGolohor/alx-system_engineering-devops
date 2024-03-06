585745c344595f90fff8295937ca4bec07b100f1
zeusgolohor <zeusgolohor@gmail.com>
2024-03-06 06:42:24 -0800

    Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
    Hint:
    
    strace can attach to a current running process
    You can use tmux to run strace in one window and curl in another one
    Requirements:
    
    Your 0-strace_is_your_friend.pp file must contain Puppet code
    You can use whatever Puppet resource type you want for you fix
    Example:

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet file to find out why Apache is returning a 500 error
+
+exec {'WordPress Server FIx',
+  command => "sudo sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  provider => shell
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+ALX SE - 0x17-web_stack_debugging_3
