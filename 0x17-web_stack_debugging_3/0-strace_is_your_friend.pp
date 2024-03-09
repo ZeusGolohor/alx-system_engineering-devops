5f5d0421288b6e6ff7a88227f973e41bf463d16d
Golohor Ufuoma (Zeus) <zeusgolohor@gmail.com>
2024-03-09 14:20:06 +0100

    Update 0-strace_is_your_friend.pp from GitHub

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # A puppet file to find out why Apache is returning a 500 error
 
 exec {'WordPress Server FIx',
-  command => 'bash -c "sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-  /var/www/html/wp-settings.php; sudo service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'l
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+  /var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
